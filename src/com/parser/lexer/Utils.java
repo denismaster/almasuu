befc213e69abc0031798498d760085cabc857887
denismaster <denismaster@outlook.com>
2017-09-04 23:15:03 +0300

    Добавлен список поддерживаемых регистров в класс Utils.

--- src/com/parser/lexer/Utils.java
+++ src/com/parser/lexer/Utils.java
@@ -8,4 +8,15 @@ public class Utils {
             "JAE",
             "TEST"
     };
+
+    public final static String[] Registers = {
+            "AX","AH","AL",
+            "BX","BH","BL",
+            "CX","CH","CL",
+            "DX","DH","DL",
+            "SI","DI",
+            "F",
+            "CS","DS","ES","SS",
+            "IP"
+    };
 }
