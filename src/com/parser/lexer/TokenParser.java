ece32732ad36675c9d631f0968871ceb0b4df43b
denismaster <denismaster@outlook.com>
2017-09-05 12:03:10 +0300

    Добавлен метод loadFile. Он используется для загрузки файла с текстом в массив строк.

--- src/com/parser/lexer/TokenParser.java
+++ src/com/parser/lexer/TokenParser.java
@@ -3,6 +3,7 @@ package com.parser.lexer;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
 public class TokenParser {
     private final String COMMENT_START_SYMBOL = ";";
@@ -15,4 +16,29 @@ public class TokenParser {
         }
         return tokenLines;
     }
+
+    private List<String> loadFile(String fileName) {
+        List<String> list = new ArrayList<>();
+        try {
+            File file = new File(fileName);
+            //создаем объект FileReader для объекта File
+            FileReader fr = new FileReader(file);
+            //создаем BufferedReader с существующего FileReader для построчного считывания
+            BufferedReader reader = new BufferedReader(fr);
+            // считаем сначала первую строку
+            String line = reader.readLine();
+            while (line != null) {
+                list.add(line);
+                line = reader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            return list;
+        }
+
+    }
+
 }
