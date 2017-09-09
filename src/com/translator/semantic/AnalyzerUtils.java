f60fd3cb5ce35f00ba0ffe067f0993d4ea61a286
denismaster <denismaster@outlook.com>
2017-09-09 23:09:21 +0300

    Добавлен метод для перевода числа в бинарный код по следующим правилам: 1) Находим старший и младший байт числа 2а) Если старший байт 0, то выводим младший 2б) Иначе выводим младший, а потом старший

--- src/com/translator/semantic/AnalyzerUtils.java
+++ src/com/translator/semantic/AnalyzerUtils.java
@@ -9,6 +9,23 @@ public class AnalyzerUtils {
         return register;
     }
 
+
+    public static String getLHValueFromInt(int in){
+        byte[] data = new byte[2]; // <- assuming "in" value in 0..65535 range and we can use 2 bytes only
+
+        data[0] = (byte)(in & 0xFF);
+        data[1] = (byte)((in >> 8) & 0xFF);
+
+        int high = data[1] >= 0 ? data[1] : 256 + data[1];
+        int low = data[0] >= 0 ? data[0] : 256 + data[0];
+
+        if(high==0){
+            return byteToString((byte)low);
+        }
+
+        return byteToString((byte)low)+" "+ byteToString((byte)high);
+    }
+
     public static String byteToString(byte value){
         return String.format("%8s", Integer.toBinaryString(value & 0xFF)).replace(' ', '0');
     }
