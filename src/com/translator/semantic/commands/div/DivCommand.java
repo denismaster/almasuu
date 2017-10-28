fa6ff31fad7c62d402ab5749edc04d4353494566
Denis Obolensky <denis_obolensky@xrm.ru>
2017-10-28 11:39:04 +0300

    Добавлена DIV-команда

--- src/com/translator/semantic/commands/div/DivCommand.java
+++ src/com/translator/semantic/commands/div/DivCommand.java
@@ -1,10 +1,26 @@
 package com.translator.semantic.commands.div;
 
+import com.translator.semantic.AnalyzerUtils;
+import com.translator.semantic.ModeType;
 import com.translator.semantic.commands.Command;
 
 public class DivCommand implements Command {
+    private boolean isWide;
+    private String register;
+    private ModeType mod;
+
+    public DivCommand(boolean isWide, String register, ModeType mod){
+        this.isWide = isWide;
+        this.register = register;
+        this.mod = mod;
+    }
     @Override
     public String generateCode() {
-        return null;
+        String opCode =  "1111011" + (isWide ? "1":"0");
+        String paramByte =
+                AnalyzerUtils.getModValue(this.mod)
+                +"110"
+                + AnalyzerUtils.getRegisterCode(register.toUpperCase());
+        return opCode+ " "+ paramByte;
     }
 }
