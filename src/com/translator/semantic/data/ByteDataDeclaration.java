bfae2c7a09abf3052325af4a10ef705bc86224a3
Denis Obolensky <denis_obolensky@xrm.ru>
2017-12-08 18:43:45 +0300

    Файлы деклараций

---
+++ src/com/translator/semantic/data/ByteDataDeclaration.java
@@ -0,0 +1,6 @@
+package com.translator.semantic.data;
+
+public class ByteDataDeclaration {
+    public String name;
+    public int value;
+}
---
+++ src/com/translator/semantic/data/WordDataDeclaration.java
@@ -0,0 +1,6 @@
+package com.translator.semantic.data;
+
+public class WordDataDeclaration {
+    public String name;
+    public int value;
+}
